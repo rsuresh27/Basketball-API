8067286575e0c131005054a6ad792ad4c25e515b
Raghav Suresh <raghav.suresh27@gmail.com>
2022-06-13 19:05:43 -0500

    Created separate services and deployments for selenium hub and nodes. Created separate deployment for api

--- Base Classes/LiveScoreBase.cs
+++ Base Classes/LiveScoreBase.cs
@@ -52,7 +52,7 @@ namespace Basketball_API.Base_Classes
 
             var random = new Random();
 
-            using (IWebDriver webDriver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), options.ElementAtOrDefault(random.Next(options.Count))))
+            using (IWebDriver webDriver = new RemoteWebDriver(new Uri("http://selenium-hub:4444/wd/hub"), options.ElementAtOrDefault(random.Next(options.Count))))
             {
                 webDriver.Manage().Timeouts().PageLoad.Add(TimeSpan.FromSeconds(30));
 
